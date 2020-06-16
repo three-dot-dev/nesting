6b2e757f56e9b69c0d477fc39473b48af493dcfb
kuetabby <vansmuhammad@gmail.com>
2020-06-16 19:51:24 +0700

    update: modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,32 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { GraphQLModule } from '@nestjs/graphql';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from "@nestjs/common"
+import { AppController } from "./app.controller"
+import { AppService } from "./app.service"
+import { GraphQLModule } from "@nestjs/graphql"
+import { TypeOrmModule } from "@nestjs/typeorm"
 
-import { UserModule } from './user/user.module';
+import { UserModule } from "./user/user.module"
 
 @Module({
-  imports: [
-    UserModule,
-    GraphQLModule.forRoot({
-      autoSchemaFile: 'schema.gql',
-      debug: true,
-      playground: true,
-    }),
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: 'localhost',
-    //   port: 5432,
-    //   username: 'postgres',
-    //   password: 'kuetabby',
-    //   database: 'nest',
-    //   entities: ['dist/**/*.model.js'],
-    //   synchronize: false,
-    // }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        UserModule,
+        GraphQLModule.forRoot({
+            autoSchemaFile: "schema.gql",
+            debug: true,
+            playground: true
+        })
+        // TypeOrmModule.forRoot({
+        //     type: "postgres",
+        //     host: "localhost",
+        //     port: 5432,
+        //     username: "postgres",
+        //     password: "",
+        //     database: "nesting",
+        //     entities: ["dist/**/*.model.js"],
+        //     synchronize: true,
+        //     logging: true
+        // })
+    ],
+    controllers: [AppController],
+    providers: [AppService]
 })
 export class AppModule {}
